181ee9ae217563168c07d915cd8446b03533d869
Henry Lin <henry.ys.lin@gmail.com>
2024-05-07 21:40:18 +0800

    Fix one mixin error

--- forge/src/main/java/com/copycatsplus/copycats/forge/mixin/compat/registrate/CreateRegistrateMixin.java
+++ forge/src/main/java/com/copycatsplus/copycats/forge/mixin/compat/registrate/CreateRegistrateMixin.java
@@ -7,9 +7,10 @@ import net.minecraftforge.registries.RegistryObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(CreateRegistrate.class)
+@Mixin(value = CreateRegistrate.class, remap = false)
 public class CreateRegistrateMixin implements CreateRegistrateAccessor {
-    @Shadow protected RegistryObject<CreativeModeTab> currentTab;
+    @Shadow
+    private RegistryObject<CreativeModeTab> currentTab;
 
     @Override
     public void copycats$setCreativeTab(RegistryObject<CreativeModeTab> tab) {
