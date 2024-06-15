b125c9adf5b6680d23eec8ae775d2a20b10f0a56
Henry Lin <henry.ys.lin@gmail.com>
2024-06-15 13:16:30 +0800

    Improve copycat board model to display each face in full

--- common/src/main/java/com/copycatsplus/copycats/content/copycat/board/CopycatMultiBoardModel.java
+++ common/src/main/java/com/copycatsplus/copycats/content/copycat/board/CopycatMultiBoardModel.java
@@ -17,53 +17,42 @@ import static com.copycatsplus.copycats.content.copycat.board.CopycatBoardBlock.
 
 public class CopycatMultiBoardModel implements SimpleMultiStateCopycatPart {
 
+    private static int i(boolean b) {
+        return b ? 1 : 0;
+    }
+
     @Override
     public void emitCopycatQuads(String key, BlockState state, CopycatRenderContext context, BlockState material) {
-        Map<Direction, Boolean> topEdges = new HashMap<>();
-        Map<Direction, Boolean> bottomEdges = new HashMap<>();
-        Map<Direction, Boolean> leftEdges = new HashMap<>();
-
-        for (Direction direction : Iterate.horizontalDirections) {
-            topEdges.put(direction, false);
-            bottomEdges.put(direction, false);
-            leftEdges.put(direction, false);
+        Map<Direction, Boolean> sides = new HashMap<>();
+        for (Direction direction : Iterate.directions) {
+            sides.put(direction, state.getValue(byDirection(direction)));
         }
 
-        for (Direction direction : Iterate.directions) {
-            BooleanProperty prop = byDirection(direction);
-            if (state.getValue(prop))
-                if (direction.getAxis().isVertical()) {
-                    Map<Direction, Boolean> edges = direction == Direction.DOWN ? bottomEdges : topEdges;
-                    int north = !edges.get(Direction.NORTH) ? 1 : 0;
-                    int south = !edges.get(Direction.SOUTH) ? 1 : 0;
-                    int east = !edges.get(Direction.EAST) ? 1 : 0;
-                    int west = !edges.get(Direction.WEST) ? 1 : 0;
-                    if (north == 1) edges.put(Direction.NORTH, true);
-                    if (south == 1) edges.put(Direction.SOUTH, true);
-                    if (east == 1) edges.put(Direction.EAST, true);
-                    if (west == 1) edges.put(Direction.WEST, true);
-                    if (key.equals(prop.getName()))
-                        assemblePiece(context, 0, direction == Direction.UP,
-                                vec3(1 - west, 0, 1 - north),
-                                aabb(14 + east + west, 1, 14 + north + south).move(1 - west, 0, 1 - north),
-                                cull(NORTH * (1 - north) | SOUTH * (1 - south) | EAST * (1 - east) | WEST * (1 - west))
-                        );
-                } else {
-                    int up = !topEdges.get(direction) ? 1 : 0;
-                    int down = !bottomEdges.get(direction) ? 1 : 0;
-                    int left = !leftEdges.get(direction) ? 1 : 0;
-                    int right = !leftEdges.get(direction.getCounterClockWise()) ? 1 : 0;
-                    if (up == 1) topEdges.put(direction, true);
-                    if (down == 1) bottomEdges.put(direction, true);
-                    if (left == 1) leftEdges.put(direction, true);
-                    if (right == 1) leftEdges.put(direction.getCounterClockWise(), true);
-                    if (key.equals(prop.getName()))
-                        assemblePiece(context, (int) direction.toYRot() + 180, false,
-                                vec3(1 - right, 1 - down, 0),
-                                aabb(14 + left + right, 14 + up + down, 1).move(1 - right, 1 - down, 0),
-                                cull(UP * (1 - up) | DOWN * (1 - down) | EAST * (1 - left) | WEST * (1 - right))
-                        );
-                }
+        Direction direction = Direction.byName(key.toLowerCase());
+
+        if (!sides.get(direction))
+            return;
+
+        if (direction.getAxis().isVertical()) {
+            assemblePiece(context, 0, direction == Direction.UP,
+                    vec3(0, 0, 0),
+                    aabb(16, 1, 16),
+                    cull((NORTH * i(sides.get(Direction.NORTH))) |
+                            (SOUTH * i(sides.get(Direction.SOUTH))) |
+                            (EAST * i(sides.get(Direction.EAST))) |
+                            (WEST * i(sides.get(Direction.WEST)))
+                    )
+            );
+        } else {
+            assemblePiece(context, (int) direction.toYRot() + 180, false,
+                    vec3(0, 0, 0),
+                    aabb(16, 16, 1),
+                    cull((UP * i(sides.get(Direction.UP))) |
+                            (DOWN * i(sides.get(Direction.DOWN))) |
+                            (EAST * i(sides.get(Direction.EAST))) |
+                            (WEST * i(sides.get(Direction.WEST)))
+                    )
+            );
         }
     }
 }
