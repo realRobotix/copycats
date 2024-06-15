867c39606941672ab6396ea52b99052bf7abfa2e
Henry Lin <henry.ys.lin@gmail.com>
2024-06-16 00:04:00 +0800

    Base model for vertical stairs

--- common/src/main/java/com/copycatsplus/copycats/content/copycat/vertical_stairs/CopycatVerticalStairsModel.java
+++ common/src/main/java/com/copycatsplus/copycats/content/copycat/vertical_stairs/CopycatVerticalStairsModel.java
@@ -3,6 +3,7 @@ package com.copycatsplus.copycats.content.copycat.vertical_stairs;
 import com.copycatsplus.copycats.CCBlockStateProperties.VerticalStairShape;
 import com.copycatsplus.copycats.content.copycat.base.model.SimpleCopycatPart;
 import com.copycatsplus.copycats.content.copycat.base.model.assembly.GlobalTransform;
+import com.copycatsplus.copycats.content.copycat.stairs.CopycatStairsModel;
 import net.minecraft.world.level.block.state.BlockState;
 
 import static com.copycatsplus.copycats.content.copycat.base.model.assembly.Assembler.*;
@@ -16,17 +17,19 @@ public class CopycatVerticalStairsModel implements SimpleCopycatPart {
 
         switch (shape) {
             case STRAIGHT -> {
-                GlobalTransform transform = t -> t.rotateY(facing);
+                GlobalTransform transform = t -> t.rotateX(90).rotateZ(90).rotateY(facing);
+                CopycatStairsModel.assembleStraight(context, transform);
             }
             case INNER_BOTTOM, INNER_TOP -> {
                 boolean flipY = shape == VerticalStairShape.INNER_TOP;
-                GlobalTransform transform = t -> t.rotateY(facing).flipY(flipY);
+                GlobalTransform transform = t -> t.rotateX(90).rotateZ(90).flipY(flipY).rotateY(facing);
+                CopycatStairsModel.assembleInnerLeft(context, transform);
             }
-            case INNER_RIGHT -> {
-            }
-            case OUTER_LEFT -> {
-            }
-            case OUTER_RIGHT -> {
+            case OUTER_BOTTOM_LEFT, OUTER_BOTTOM_RIGHT, OUTER_TOP_LEFT, OUTER_TOP_RIGHT -> {
+                boolean flipY = shape == VerticalStairShape.OUTER_TOP_LEFT || shape == VerticalStairShape.OUTER_TOP_RIGHT;
+                boolean flipX = shape == VerticalStairShape.OUTER_BOTTOM_RIGHT || shape == VerticalStairShape.OUTER_TOP_RIGHT;
+                GlobalTransform transform = t -> t.rotateX(90).rotateZ(90).flipX(flipX).flipY(flipY).rotateY(facing);
+                CopycatStairsModel.assembleOuterLeft(context, transform);
             }
         }
     }
